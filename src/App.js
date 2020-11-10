0ea687266828ec2eba2ea9b3d8c9435a45f62842
Ryan-Wisniewski <Ryanwis98@yahoo.com>
2020-11-10 13:57:44 -0500

    corrected url for deployment

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, {useState, useEffect} from 'react';
 import io from 'socket.io-client'
 import NavBar from './NavBar'
 import './App.css';
-// const url = 'https://snake-online-serve.herokuapp.com/'
-const url = 'localhost:8000'
+const url = 'https://snake-online-serve.herokuapp.com/'
+// const url = 'localhost:8000'
 const socket = io.connect(url);
 function App() {
   const [userId, setUserId] = useState()
